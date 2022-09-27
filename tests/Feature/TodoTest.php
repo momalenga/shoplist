62180fa202e5dbf2e5b570be5b157adf684d8069
Mo Malenga <mo@shengamo.com>
2022-09-27 13:06:31 +0200

    added the delete dB

--- tests/Feature/TodoTest.php
+++ tests/Feature/TodoTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Http\Livewire\TodoComponent;
+use App\Models\Todo;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 use Tests\TestCase;
@@ -38,4 +39,42 @@ class TodoTest extends TestCase
         // submit item and checked.
         $this->assertDatabaseHas('todos', ['item' => 'Ice cream']);
     }
+
+    public function test_a_user_can_edit_a_todo()
+    {
+        // Login
+        $this->login();
+        $todo = Todo::factory()->create(['item' => 'goldfish']);
+
+        // Confirm the info is in db
+        $this->assertEquals('goldfish', Todo::first()->item);
+        $this->assertNotEquals('Ice cream', Todo::first()->item);
+
+        // Got to the right Livewire component
+        Livewire::test(TodoComponent::class)
+            ->call('editTodo', $todo)
+            ->set('todo.item', 'Ice cream')
+            ->call('saveTodo');
+
+        // submit item and checked.
+        $this->assertEquals('Ice cream', Todo::first()->item);
+    }
+
+    // Uncomment this test for deleting the DB
+    // public function test_a_user_can_delete_a_todo()
+    // {
+    //     // Login
+    //     $this->login();
+    //     $todo = Todo::factory()->create(['item' => 'goldfish']);
+
+    //     // Confirm DB has at least one value
+    //     $this->assertEquals(1, Todo::count());
+
+    //     // Got to the right Livewire component
+    //     Livewire::test(TodoComponent::class)
+    //         ->call('deleteTodo', $todo);
+
+    //     // submit item and checked.
+    //     $this->assertEquals(0, Todo::count());
+    // }
 }
